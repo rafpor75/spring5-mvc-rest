ab1570f121248ee7a2992e9e682e28fe2ce0b077
Raffaele Porzio <37336855+rafpor75@users.noreply.github.com>
2018-03-28 11:05:51 +0200

    refactor

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,7 +1,8 @@
-package guru.springfamework.repositories;
+package guru.springframework.repositories;
 
-import guru.springfamework.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import guru.springframework.domain.Category;
 
 /**
  * Created by jt on 9/24/17.
